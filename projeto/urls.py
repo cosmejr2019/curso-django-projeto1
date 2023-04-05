65397deb73e9b4ecfcf2f4888424753fb374a4d1
Cosme Júnior <cosme_junior16@hotmail.com>
2023-04-05 18:55:24 -0300

    o

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,24 +1,22 @@
-from django.urls import path
-from django.http import HttpResponse
+"""projeto URL Configuration
 
-
-def my_view(request):
-    return HttpResponse('UM LINDA STRING')
-def home(request):
-    return HttpResponse('HOME')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('outra-coisa-qualquer/', my_view),
-    path('', home),  # Home
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
+    path('admin/', admin.site.urls),
+    path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-# from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otávio',
+    })
